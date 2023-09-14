06fc71180cf5822cbcf9a2ab7be0f240489ff6d2
PedroAlmeida08 <74750373+PedroAlmeida08@users.noreply.github.com>
2023-09-14 17:44:14 -0300

    Create main.dart

---
+++ Aula01/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Hello App",
+    debugShowCheckedModeBanner: false,
+    home: Container(
+      padding: EdgeInsets.only(left: 30, top: 20),
+      margin: EdgeInsets.all(15),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Hello World!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold)),
+          Text("Texto 2"),
+          Text("Texto 3"),
+          ElevatedButton(
+              /*onPressed: () {
+                print("Cliquei!");
+              }, // Função Anônima*/
+              onPressed: () => (print("Cliquei!")), //Arrow Function
+              child: Text("Clique aqui!"))
+        ],
+      ),
+      decoration:
+          BoxDecoration(border: Border.all(color: Colors.blue, width: 10)),
+    ),
+  ));
+}
