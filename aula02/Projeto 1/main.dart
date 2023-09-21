d73b88b7a0e2595b6ff8f10d221f2dc7c2205beb
PedroAlmeida08 <74750373+PedroAlmeida08@users.noreply.github.com>
2023-09-21 17:34:10 -0300

    Update main.dart

--- aula02/Projeto 1/main.dart
+++ aula02/Projeto 1/main.dart
@@ -1,28 +1,10 @@
-import 'dart:html';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:projeto_1/home.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'Frases Aleatórias',
     debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(title: Text('Frases Aleatórias')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Image.asset("images/title.png"),
-            Text("Clique aqui para gerar uma frase.",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Nova Frase"),
-            )
-          ],
-        ),
-      ),
-    ),
+    home: Home(),
   ));
 }
