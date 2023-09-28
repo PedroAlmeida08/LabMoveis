fcdb8f10cfe79751fff2c9cd9add2251d253bc03
PedroAlmeida08 <74750373+PedroAlmeida08@users.noreply.github.com>
2023-09-28 17:40:30 -0300

    Create main.dart

---
+++ aula03/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Entrada de Dados',
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
